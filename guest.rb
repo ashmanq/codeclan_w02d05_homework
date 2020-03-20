8cf6811d01c65abdc52817ca9aee410a2a97f95e
Ashir <ashirq@outlook.com>
2020-03-20 17:06:33 +0000

    added wallets to guests

--- guest.rb
+++ guest.rb
@@ -1,10 +1,21 @@
 
 class Guest
 
-  attr_reader :name
-  
-  def initialize(name)
+  attr_reader :name, :wallet
+
+  def initialize(name, money)
     @name = name
+    @wallet = money
+  end
+
+  def get_money(amount)
+    @wallet += amount
+  end
+
+  def give_money(amount)
+    if amount <= @wallet
+      @wallet -= amount
+    end
   end
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -9,11 +9,34 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 class GuestTest < Minitest::Test
 
   def setup
-    @guest1 = Guest.new("James Jameson")
+    @guest1 = Guest.new("James Jameson", 100)
   end
 
   def test_guest_has_name()
     assert_equal("James Jameson", @guest1.name)
   end
 
+  def test_guest_starting_wallet_money()
+    assert_equal(100, @guest1.wallet)
+  end
+
+  def test_add_to_wallet()
+    @guest1.get_money(10)
+    assert_equal(110, @guest1.wallet())
+  end
+
+  def test_give_money__enough_in_wallet()
+    @guest1.give_money(90)
+    assert_equal(10, @guest1.wallet())
+  end
+
+  def test_give_money__not_enough_in_wallet()
+    @guest1.give_money(200)
+    assert_equal(100, @guest1.wallet())
+  end
+
+  def test_give_money__just_enough_in_wallet()
+    @guest1.give_money(100)
+    assert_equal(0, @guest1.wallet())    
+  end
 end
